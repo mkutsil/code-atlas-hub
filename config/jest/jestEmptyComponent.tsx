826f185e5ad45f6b8c4151d10be9b689625e8a21
Mykola Kutsil <mkutsil.dev@gmail.com>
2025-03-17 20:12:15 +0200

    fix: deleted unused variables

--- config/jest/jestEmptyComponent.tsx
+++ config/jest/jestEmptyComponent.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { TextEncoder, TextDecoder } from 'util';
 
 const jestEmptyComponent = () => <div/>;
-
-Object.assign(global, { TextDecoder, TextEncoder });
 
 export default jestEmptyComponent;
