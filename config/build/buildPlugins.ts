be022279459d04ca66b7fbf8e9c97b509810aa92
Mykola Kutsil <mkutsil.dev@gmail.com>
2025-03-20 15:30:14 +0200

    fix: fixed config

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,7 +4,6 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { DefinePlugin, HotModuleReplacementPlugin, ProgressPlugin, WebpackPluginInstance } from 'webpack';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
-  // test 
 
 export function buildPlugins({ paths, isDev }: BuildOptions): WebpackPluginInstance[] {
     return [
