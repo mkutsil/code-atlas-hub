d34323cf4f4d326aad8dcba6509935ac6f2e1f25
Mykola Kutsil <mkutsil.dev@gmail.com>
2025-03-26 16:57:15 +0200

    feature: added grid table

--- src/pages/HomePage/ui/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -1,11 +1,14 @@
 import { BugButton } from 'app/providers/ErrorBoundary';
 import classes from './HomePage.module.scss';
+import GridTable from './components/GridTable/GridTable';
 
 const HomePage = () => (
     <div className={classes.homePageContainer}>
         <h1>CodeAtlasHub</h1>
 
         <div><BugButton/></div>
+       
+        <GridTable/>
     </div>
 );
 
---
+++ src/pages/HomePage/ui/components/GridTable/GridTable.tsx
@@ -0,0 +1,25 @@
+import classes from './GridTable.module.scss';
+import { tableData } from './tableData';
+
+const GridTable = () => ( 
+    <>
+        <h1>Table</h1>
+
+        <div className={classes.gridContainer}>
+            {tableData.headers.map((tableItem, index) => (
+                <div key={index} className={classes.headerGrid}>
+                    {tableItem}
+                </div>
+            ))}
+            {tableData.rows.flatMap((row) => 
+                row.map((cell, colIndex) => (
+                    <div key={colIndex}>
+                        {cell}
+                    </div>
+                ))
+            )}
+        </div>
+    </>
+	 );
+ 
+export default GridTable;
