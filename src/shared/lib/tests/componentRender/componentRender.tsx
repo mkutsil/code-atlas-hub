b87c194a553399109cef0eff24f8ff61931561c1
Mykola Kutsil <mkutsil.dev@gmail.com>
2025-04-08 17:31:43 +0300

    feature: added instans axios api

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 import { render } from '@testing-library/react';
 import { I18nextProvider } from 'react-i18next';
-import { MemoryRouter } from 'react-router-dom';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
 import i18nForTests from 'shared/config/i18n/i18nForTest';
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
 
@@ -14,12 +14,14 @@ export const componentRender = (component: ReactNode, options: componentRenderOp
     const { route = '/', initialState={} } = options;
     
     return render(
-        <StoreProvider initialState={initialState}>
-            <MemoryRouter initialEntries={[ route ]}>
-                <I18nextProvider i18n={i18nForTests}>
-                    {component}
-                </I18nextProvider>
-            </MemoryRouter>
-        </StoreProvider>
+        <BrowserRouter>
+            <StoreProvider initialState={initialState}>
+                <MemoryRouter initialEntries={[ route ]}>
+                    <I18nextProvider i18n={i18nForTests}>
+                        {component}
+                    </I18nextProvider>
+                </MemoryRouter>
+            </StoreProvider>
+        </BrowserRouter>
     );
 };
