cb7c7206699469cd365b2ef4b48be9b3b52b8db2
Mykola Kutsil <mkutsil.dev@gmail.com>
2025-05-02 16:06:11 +0300

    feature: added profile view form

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -10,12 +10,9 @@ import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import DynamicModuleLoader, { ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch';
-import Button, { ThemeButton } from 'shared/ui/Button/Button';
-import Input from 'shared/ui/Input/Input';
 import Loader from 'shared/ui/Loader/Loader';
-import classes from './ProfilePage.module.scss';
-import Avatar, { AvatarSize } from 'shared/ui/Avatar/Avatar';
 import ProfileEditForm from './components/ProfileEditForm/ProfileEditForm';
+import ProfileViewForm from './components/ProfileViewForm/ProfileViewForm';
 
 interface ProfilePageProps {
 	id?: string
@@ -44,74 +41,13 @@ const ProfilePage = (props: ProfilePageProps) => {
     return ( 
         <DynamicModuleLoader reducers={reducers}>
             <div key={id}>
-                <h1>Profile Page</h1>
-
-                {readonly && (
-                    <Button 
-                        theme={ThemeButton.CONTAINED} 
-                        onClick={() => onChangeEditMode(!readonly)}
-                    >
-                        Edit
-                    </Button>
-                )}
-               
                 {isLoading ? <Loader/> : (
                     <>
                         {readonly ? (
-                            <div className={classes.profileViewContainer}>
-                                <div className={classes.profileCardWrapper}>
-
-                                    <Avatar 
-                                        src={profileData?.avatar || ''}
-                                        alt="Avatar"
-                                        size={AvatarSize.LARGE}
-                                    />
-
-                                    <div>
-                                        <span>
-                                            {profileData?.firstName} {profileData?.lastName}
-                                        </span>
-                                        
-                                    </div>
-                                </div>
-                                <div className={classes.viewInputsContainer}>
-                                    <Input 
-                                        value={profileData?.firstName} 
-                                        placeholder="First Name" 
-                                        isViewMode 
-                                    />
-                                    <Input 
-                                        value={profileData?.lastName} 
-                                        placeholder="Last Name" 
-                                        isViewMode 
-                                    />
-                                    <Input 
-                                        value={profileData?.age.toString()} 
-                                        placeholder="Age" 
-                                        isViewMode 
-                                    />
-                                    <Input 
-                                        value={profileData?.currency} 
-                                        placeholder="Currency" 
-                                        isViewMode
-                                    />
-                                    <Input 
-                                        value={profileData?.country} 
-                                        placeholder="Country" 
-                                        isViewMode
-                                    />
-                                    <Input 
-                                        value={profileData?.city}
-                                        placeholder="City" 
-                                        isViewMode 
-                                    />
-                                    <Input 
-                                        value={profileData?.userName} 
-                                        placeholder="User Name" 
-                                        isViewMode 
-                                    />
-                                </div>
-                            </div>
+                            <ProfileViewForm
+                                profileData={profileData}
+                                onChangeEditMode={onChangeEditMode}
+                            />
                         ) : (
                             <ProfileEditForm 
                                 profileData={profileData}
