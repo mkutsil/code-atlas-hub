49734d7d14f0657be84d540277829597a593ee8c
Mykola Kutsil <mkutsil.dev@gmail.com>
2025-07-11 01:35:07 +0300

    fix: fixed favicon path

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,9 +1,11 @@
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { DefinePlugin, HotModuleReplacementPlugin, ProgressPlugin, WebpackPluginInstance } from 'webpack';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
+import path from 'path';
 
 export function buildPlugins({ paths, isDev, apiUrl }: BuildOptions): WebpackPluginInstance[] {
     return [
@@ -15,6 +17,16 @@ export function buildPlugins({ paths, isDev, apiUrl }: BuildOptions): WebpackPlu
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css'
         }),
+        new CopyWebpackPlugin({
+            patterns: [
+              {
+                from: path.resolve(__dirname, '..', '..', 'public', 'logos'),
+                to: 'logos',
+              },
+            ],
+          }),
+          
+          
         new DefinePlugin({
             IS_DEV: JSON.stringify(isDev),
             API_URL: JSON.stringify(apiUrl),
